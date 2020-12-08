03b11ea4092f10b6dae0fd8d88e22507c57b4228
panas224 <panas.oleh2019@gmail.com>
2020-12-08 19:26:45 +0200

    Added Search People

--- SocialNetwork/SocialNetwork/SearchPeople/PersonInfo.xaml.cs
+++ SocialNetwork/SocialNetwork/SearchPeople/PersonInfo.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SocialNetwork.SearchPeople
 {
     /// <summary>
-    /// Interaction logic for PersonWindow.xaml
+    /// Interaction logic for PersonInfo.xaml
     /// </summary>
-    public partial class PersonWindow : Page
+    public partial class PersonInfo : Window
     {
-        public PersonWindow()
+        public PersonInfo()
         {
             InitializeComponent();
         }
