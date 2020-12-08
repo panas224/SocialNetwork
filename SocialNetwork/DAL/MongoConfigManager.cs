99308714f5d9764fd82eb6a7e146cf66bae17ad1
panas224 <panas.oleh2019@gmail.com>
2020-12-08 20:54:40 +0200

    Added classes Working with database

--- SocialNetwork/DAL/MongoConfigManager.cs
+++ SocialNetwork/DAL/MongoConfigManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    class MongoConfigManager
+    public class MongoConfigManager
     {
+        private MongoConfigManager()
+        {
+        }
+
+        public static IMongoDatabase GetDefaultDatabase()
+        {
+            var connectionString = GetDefaultConnectionString();
+            var client = new MongoClient(connectionString);
+            return client.GetDatabase(GetDefaultDatabaseName());
+        }
+
+        private static string GetDefaultConnectionString()
+        {
+            return "mongodb://localhost:27017";
+        }
+
+
+        private static string GetDefaultDatabaseName()
+        {
+            return "test";
+        }
+
+
     }
 }
